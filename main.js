93d4f8d79f01269cc4410899b60c1618bd2a9fee
Guilherme Lupe Jorge <92446719+Gui1127@users.noreply.github.com>
2022-11-07 21:30:51 -0300

    Add files via upload
    transições adicionadas

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ function createGame (player1, hour, player2 ) {
   `
 }
 
-
+let delay = -0.3;
 function createCard (date, day, games){
+  delay= delay + 0.3;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -20,24 +21,17 @@ function createCard (date, day, games){
   `
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-      <img src="./assets/logo.svg" alt="Logo">
-    </header>
-    <main id="cards">
-      ${createCard ("20/11", "domingo",
-        createGame ("qatar", "13:00", "ecuador")
-        )}
-      ${createCard ("21/11", "segunda",
-        createGame ("england", "10:00", "iran") +
-        createGame ("senegal", "13:00", "netherlands") +
-        createGame ("usa", "16:00", "wales")
-        )}
-      ${createCard ("22/11", "terça",
-        createGame ("argentina", "07:00", "arabia") +
-        createGame ("denmark", "10:00", "tunisia") +
-        createGame ("mexico", "13:00", "poland") +
-        createGame ("france", "16:00", "australia")
-        )}
-    </main>
-`
+document.querySelector("#cards").innerHTML = 
+  createCard ("20/11", "domingo", 
+    createGame ("qatar", "13:00", "ecuador")) +
+
+  createCard ("21/11", "segunda", 
+    createGame ("england", "10:00", "iran")+ 
+    createGame ("senegal", "13:00", "netherlands") + 
+    createGame ("usa",  "16:00", "wales")) +
+      
+  createCard ("22/11", "terça", 
+    createGame ("argentina", "07:00", "arabia") + 
+    createGame ("denmark", "10:00", "tunisia") + 
+    createGame ("mexico", "13:00", "poland") + 
+    createGame ("france", "16:00", "australia"))
